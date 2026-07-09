9afa5cf333795b04e5774959c24617fa0997097f
Awais <awais@gmail.com>
2026-07-09 11:43:42 +0500

    changes

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ const app = express();
 // Configure multer for file uploads
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');
+    cb(null, path.join(__dirname, '../uploads'));
   },
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
